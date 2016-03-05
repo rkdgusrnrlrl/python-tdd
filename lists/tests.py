2949b70bbac73aa0be7075716e4dd201aaaf170c
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-03-05 12:55:35 +0900

    뷰렌딩 까지 성공 기능 테스트(test_enter_website) 성공

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 from lists.views import home_page
 
@@ -7,3 +8,10 @@ class HomePageTest(TestCase):
     def test_root_exist(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
