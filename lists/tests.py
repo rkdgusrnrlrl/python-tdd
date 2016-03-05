23a76bbd0a7bb610b8e848157c0a3684c0ca2d02
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-03-05 14:44:38 +0900

    template 을 활용해 뷰 렌더링 리펙토링 위의 template을 이용 하여 테스트 코드 변경

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from lists.views import home_page
 
@@ -12,6 +13,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expect_html = render_to_string('home.html')
+        #content 는 바이트 문자열임으로 decode를 통해 파이썬 유니코드문자열로 변환ㅊ
+        self.assertEqual(response.content.decode(), expect_html)
+
