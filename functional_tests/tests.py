f4fe9ff1152dfa50a4e453a0240e313ff92e38d0
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-03-15 21:37:14 +0900

    input 가운데 정렬 테스트 추가

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -81,3 +81,23 @@ class NewVistorTest(LiveServerTestCase):
         inputbox.send_keys(todo01)
         inputbox.send_keys(Keys.ENTER)
 
+    def test_layout_and_styling(self):
+        #에디스는 메인 페이지를 방문한다.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #그녀는 입력 상사가 가운데 배치 된것을 본다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
