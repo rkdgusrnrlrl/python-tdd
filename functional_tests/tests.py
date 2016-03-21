6b23bed705ee6b909cb5de89c20c889ac275600b
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-03-21 21:49:07 +0900

    스테이징 서버 배포를 위한 테스트 작성

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -7,6 +8,16 @@ from selenium.webdriver.common.keys import Keys
 
 
 class NewVistorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         #암묵적인 대기
@@ -17,7 +28,7 @@ class NewVistorTest(StaticLiveServerTestCase):
         self.browser.quit()
 
     def test_enter_website(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.assertIn('To-Do' , self.browser.title)
 
         #head_text = self.browser.find_element_by_tag_name('h1').text
@@ -52,7 +63,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 
         #프란시스가 홈페이지에 접속한다.
         #에디스의 리스트는 보이지 않는다.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn(todo01, page_text)
         self.assertNotIn(todo02, page_text)
@@ -85,7 +96,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         #에디스는 메인 페이지를 방문한다.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         #그녀는 입력 상사가 가운데 배치 된것을 본다.
